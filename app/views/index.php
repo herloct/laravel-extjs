d34b2388532b17c790b792b7c67f6a45d7d5d41f
herloct <herloct@gmail.com>
2014-08-06 15:29:05 +0700

    Create view for Index

---
+++ app/views/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Welcome to Ext JS!</title>
+        <link rel="stylesheet" type="text/css" href="http://cdn.sencha.com/ext/gpl/5.0.0/build/packages/ext-theme-neptune/build/resources/ext-theme-neptune-all.css">
+        <script type="text/javascript" src="http://cdn.sencha.com/ext/gpl/5.0.0/build/ext-all.js"></script> 
+        <script type="text/javascript" src="http://cdn.sencha.com/ext/gpl/5.0.0/build/packages/ext-theme-neptune/build/ext-theme-neptune.js"></script>
+    </head>
+    
+    <body>
+        <script type ="text/javascript" src="app.js"></script>
+    </body>
+</html>
